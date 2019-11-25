24795318d46fc0ce0071aa56400eb8d292bf53b8
sushantd <sushantd@SushantD-Desk.talentica-all.com>
2019-11-25 18:10:39 +0530

    Day 1

--- server.php
+++ server.php
@@ -3,6 +3,7 @@ session_start();
 
 //Initialize var
 $username = "";
+$errors = array();
 
 //connect to database
 $db = mysqli_connect('localhost','root','','demo') or die('unable to connect to database');
@@ -14,4 +15,21 @@ $technology = mysqli_real_escape_string($db, $_POST['technology']);
 $username = mysqli_real_escape_string($db, $_POST['username']);
 $password = mysqli_real_escape_string($db, $_POST['password']);
 $confirmPassword = mysqli_real_escape_string($db, $_POST['confirmPassword']);
+
+//form validation
+if (empty($username)) {
+    array_push($errors, "username cannot be empty");
+}
+if(empty($grade)) {
+    array_push($errors,"grade cannot be empty");
+}
+if(empty($technology)) {
+    array_push($errors,"technology cannot be empty");
+}
+if(empty($grade)) {
+    array_push($errors,"grade cannot be empty");
+}
+if(empty($grade)) {
+    array_push($errors,"grade cannot be empty");
+}
 ?>
